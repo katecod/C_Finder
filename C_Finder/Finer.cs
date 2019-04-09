2033281df8ab40d95d085b140d4eb79d3bab1a11
katecod <kateco@de.ce>
2019-04-09 08:35:27 +0300

    Initial commit

---
+++ C_Finder/Finer.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_Finder
+{
+    class Finer
+    {
+        string[] acceptFormats = new string[] { ".txt", ".tex", ".pdf" };
+        public void Find(List<string> files, string TheWord)
+        {
+            int allresult = 0;
+            foreach (string file in files)
+            {
+                CanBeOpen canBeOpen = FormatIs(file);
+                if (canBeOpen.TakeCanI())
+                {
+                    WriteInFile("Start reading file:  " + file, true);
+                    answer answ = ToOpener(file, canBeOpen.TakeFormat(), TheWord);
+                    if (answ.ResultCount() == 0)
+                    {
+                        WriteInFile("result not found.", false);
+                    }
+                    else
+                    {
+                        allresult += answ.ResultCount();
+                        WriteInFile("found " + answ.ResultCount().ToString() + " results", false);
+                        foreach (var result in answ.AllResultsData())
+                        {
+                            WriteInFile(result, true);
+                        }
+                    }
+
+                }
+                else
+                {
+                    WriteInFile("File have wrong format:  " + file, true);
+                }
+                WriteInFile("", true);
+            }
+        }
+        private CanBeOpen FormatIs(string path)
+        {
+            CanBeOpen canBeOpen = new CanBeOpen();
+            string format = Path.GetExtension(path);
+            if (acceptFormats.Contains(format))
+            {
+                canBeOpen.AddData(true, format);
+            }
+            else
+            {
+                canBeOpen.AddData(false);
+            }
+            return canBeOpen;
+        }
+
+        private answer ToOpener(string path, string format, string TheWord)
+        {
+            answer thisansw = new answer();
+            if (format==acceptFormats[0] || format == acceptFormats[1])//.txt .tex
+            {
+                Easyread(thisansw, path,TheWord);
+            }
+            else if (format == acceptFormats[2] )//.pdf
+            {
+                PdfReader(thisansw, path, TheWord);
+            }
+            return thisansw;
+        }
+
+        private void Easyread(answer thisansw, string path, string TheWord)
+        {
+            try
+            {
+                if (File.ReadAllText(path).Contains(TheWord))
+                {
+                    string AllData = File.ReadAllText(path);
+                    AllData = GetClean(AllData);
+                    foreach (Match mtch in Regex.Matches(AllData, TheWord))
+                    {
+                        takesting tkstr = new takesting();
+                        GetIndexs(tkstr,mtch,AllData);
+                        string answ = AllData.Substring(tkstr.GetStarti(), tkstr.GetLasti());
+                        thisansw.addResult(answ);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                WriteInFile("ERROR:"+path, true);
+                throw;
+            }
+
+        }//txt, tex
+
+
+        private void PdfReader(answer thisansw, string path, string TheWord)
+        {
+            try
+            {
+                /* 
+                 * if (File.ReadAllText(path).Contains(TheWord))
+                 {
+                     string AllData = File.ReadAllText(path);
+                     AllData = GetClean(AllData);
+                     foreach (Match mtch in Regex.Matches(AllData, TheWord))
+                     {
+                         takesting tkstr = new takesting();
+                         GetIndexs(tkstr, mtch, AllData);
+                         string answ = AllData.Substring(tkstr.GetStarti(), tkstr.GetLasti());
+                         thisansw.addResult(answ);
+                     }
+                 }*/
+
+            }
+            catch (Exception)
+            {
+                WriteInFile("ERROR:" + path, true);
+                throw;
+            }
+        }//pdf
+        private void GetIndexs(takesting tkstr, Match mtch, string AllData)
+        {
+            int beforeAndAfter = 30;
+            if (mtch.Index>beforeAndAfter)
+            {
+                tkstr.PutStarti(mtch.Index - beforeAndAfter);
+            }
+            else
+            {
+                tkstr.PutStarti(0);
+            }
+            if (AllData.Length>(mtch.Length+mtch.Length+beforeAndAfter))
+            {
+                tkstr.Putlasti(mtch.Length + mtch.Length + beforeAndAfter);
+            }
+            else
+            {
+                tkstr.Putlasti(AllData.Length);
+            }
+        }
+        private string GetClean(string AllData)
+        {
+            AllData = Regex.Replace(AllData, @"\s+", " ");
+            return AllData;
+        }
+        private void WriteInFile(string line, bool newstring)
+        {
+            using (StreamWriter sw = new StreamWriter("results.txt", true, Encoding.Default))
+            {
+                if (newstring)
+                {
+                    sw.WriteLine(line);
+                }
+                else
+                {
+                    sw.Write("  " + line);
+                }
+
+            }
+        }
+    }
+    class answer
+    {
+        private int resultsWasFound = 0;
+        private List<string> Results = new List<string>();
+
+        public void addResult(string result)
+        {
+            resultsWasFound++;
+            Results.Add(result);
+        }
+        public int ResultCount()
+        {
+            return resultsWasFound;
+        }
+        public List<string> AllResultsData()
+        {
+            return Results;
+        }
+    }
+    class takesting
+    {
+        private int starti;
+        private int lasti;
+        public void PutStarti(int start) { starti = start; }
+        public void Putlasti(int End) {lasti = End; }
+        public int GetStarti() { return starti; }
+        public int GetLasti() { return lasti; }
+    }
+    class CanBeOpen
+    {
+        private bool canOpen;
+        private string format;
+        public void AddData(bool CanOpen)
+        {
+            canOpen = CanOpen;
+        }
+        public void AddData(bool CanOpen, string Format)
+        {
+            canOpen = CanOpen;
+            format = Format;
+        }
+        public bool TakeCanI()
+        {
+            return canOpen;
+        }
+        public string TakeFormat()
+        {
+            return format;
+        }
+    }
+}
---
+++ C_Finder/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace C_Finder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.el_b_start = new System.Windows.Forms.Button();
+            this.el_tb_WordToFind = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // el_b_start
+            // 
+            this.el_b_start.Location = new System.Drawing.Point(12, 38);
+            this.el_b_start.Name = "el_b_start";
+            this.el_b_start.Size = new System.Drawing.Size(316, 49);
+            this.el_b_start.TabIndex = 0;
+            this.el_b_start.Text = "Start";
+            this.el_b_start.UseVisualStyleBackColor = true;
+            this.el_b_start.Click += new System.EventHandler(this.el_b_start_Click);
+            // 
+            // el_tb_WordToFind
+            // 
+            this.el_tb_WordToFind.Location = new System.Drawing.Point(13, 13);
+            this.el_tb_WordToFind.Name = "el_tb_WordToFind";
+            this.el_tb_WordToFind.Size = new System.Drawing.Size(315, 20);
+            this.el_tb_WordToFind.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 99);
+            this.Controls.Add(this.el_tb_WordToFind);
+            this.Controls.Add(this.el_b_start);
+            this.Name = "Form1";
+            this.Text = "Finder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button el_b_start;
+        private System.Windows.Forms.TextBox el_tb_WordToFind;
+    }
+}
+
